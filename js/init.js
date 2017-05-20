e95df0d7e0941a16c64b49e96bf8a60b50e19f02
Stuart Maitland <stuart@mediwikis.com>
2017-05-20 19:13:19 +0100

    Fixed the parallax effect for the hero video, plus minor styling changes

--- js/init.js
+++ js/init.js
@@ -5,5 +5,15 @@
     $('.parallax').parallax();
     $('.modal').modal();
 
+
+
   }); // end of document ready
+
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function () {
+    $(window).on('load scroll', function () {
+            var scrolled = $(this).scrollTop();
+            $('.hero-vid').css('transform', 'translate3d(0, ' +(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+    });
+});
